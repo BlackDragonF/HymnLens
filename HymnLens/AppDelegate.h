1deb8097140990200b697554ef8f6c599f563dd2
陈志浩 <chenzhihao@chenzhihaodemacbook-air.local>
2016-01-28 11:33:41 +0800

    Initial Commit

---
+++ HymnLens/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HymnLens
+//
+//  Created by 陈志浩 on 16/1/28.
+//  Copyright © 2016年 陈志浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HymnLens/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HymnLens
+//
+//  Created by 陈志浩 on 16/1/28.
+//  Copyright © 2016年 陈志浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
