f8d8c41ab290c1eaaf7af5e1a1ee884ef0bbebeb
oneHuster <540847140@qq.com>
2016-01-29 17:21:27 +0800

    create linkView

---
+++ OperateVC.h
@@ -0,0 +1,13 @@
+//
+//  OperateVC.h
+//  HymnLens
+//
+//  Created by 邹应天 on 16/1/29.
+//  Copyright © 2016年 陈志浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OperateVC : UIViewController
+
+@end
---
+++ OperateVC.m
@@ -0,0 +1,37 @@
+//
+//  OperateVC.m
+//  HymnLens
+//
+//  Created by 邹应天 on 16/1/29.
+//  Copyright © 2016年 陈志浩. All rights reserved.
+//
+
+#import "OperateVC.h"
+
+@interface OperateVC ()
+
+@end
+
+@implementation OperateVC
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
